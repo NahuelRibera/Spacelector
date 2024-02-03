623f879be39b02e351f836a940521a64c775771e
Marti Carmona <mtkindus@gmail.com>
2024-02-03 13:26:40 +0100

    image title

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
+# app/models/image.rb
 class Image < ApplicationRecord
   belongs_to :space
   has_one_attached :file
-  # Remove the title validation if you're not using it
-  # validates :title, presence: true
+  validates :title, presence: true
 end
