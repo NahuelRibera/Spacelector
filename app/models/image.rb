f988f980ca25b1cc5ae8867a875e1cd11c69bcfe
Marti Carmona <mtkindus@gmail.com>
2024-02-13 12:52:20 +0100

    compartments

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,8 @@
 class Image < ApplicationRecord
   belongs_to :space
-  has_many :compartments
-  has_one_attached :file 
+  has_many :compartments, dependent: :destroy
+  has_one_attached :file
+  accepts_nested_attributes_for :compartments
+
   validates :title, presence: true
 end
