4e070d82e173c16728d3a9f2f3def2ebbb838b38
Marti Carmona <MTKindus@gmail.com>
2024-03-08 14:49:48 +0100

    aa

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,4 +4,14 @@ class Image < ApplicationRecord
   has_one_attached :file
   accepts_nested_attributes_for :compartments
   validates :title, presence: true
+  after_create_commit :convert_heic_to_jpeg
+
+  private
+
+  # Convert HEIC image to JPEG
+  def convert_heic_to_jpeg
+    return unless file.attached? && file.content_type == 'image/heic'
+    # Perform the conversion in a background job for performance
+    ConvertHeicToJpegJob.perform_later(file.blob.id)
+  end
 end
