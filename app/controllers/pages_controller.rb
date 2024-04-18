a897f3ce38aa1a2002d17d779c9d2d04d63ea06c
Nahuel Ribera <nriberapingitore@gmail.com>
2024-04-18 14:48:50 +0200

    terms done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :terms_and_conditions ]
 
   def home
     @portal_session = current_user.payment_processor.billing_portal
+  end
+
+  def terms_and_conditions
+    # Any logic or data retrieval for the terms and conditions page can go here
   end
 end
