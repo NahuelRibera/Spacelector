190597140c8b35af1db5181036c6effa266cac30
MTK <marticarmonaserrat@MTKs-Laptop.local>
2024-06-06 13:01:07 +0200

    render8

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install dependencies
-apt-get update -qq
-apt-get install -y build-essential libpq-dev imagemagick libheif-dev
+# Install necessary dependencies for HEIC support
+apt-get update && apt-get install -y libheif-dev
 
-# Install Ruby dependencies
 bundle install
-
-# Precompile assets and clean up
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
